3847098d7ec00b48838b31ecf1d836aa46ff60ee
rxtthin <rotthin_dev@protonmail.com>
2022-10-15 09:00:41 +0200

    Removed dist from repository

--- dist/errors/shaderProgramLinkError.js
+++
@@ -1,6 +0,0 @@
-export class ShaderProgramLinkError extends Error {
-    constructor(msg) {
-        super(`Failed to link shader program: ${msg}`);
-        alert(this.message);
-    }
-}
--- dist/helpers/random.js
+++
@@ -1,4 +0,0 @@
-export var random;
-(function (random) {
-    random.range = (min, max) => Math.random() * (max - min) + min;
-})(random || (random = {}));
